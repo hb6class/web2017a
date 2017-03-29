841f22d408f7e448e41a21f21991f9dec0102ffb
hb6class <hb.6class@gmail.com>
2017-03-29 12:46:47 +0900

    web-day12 junit 명령어소개

---
+++ day12/src/com/hb/test/TestEx01.java
@@ -0,0 +1,56 @@
+package com.hb.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestEx01 {
+
+	@Test
+	public void testFunc1() {
+		int a,b,c;
+		a=3;
+		b=2;
+		c=a+b;
+		assertSame(5, c);//==
+		assertNotSame(6, c);//==
+	}
+	@Test
+	public void testFunc2() {
+		String st1, st2;
+		st1=new String("java");
+		st2=new String("java");
+		assertEquals(st1, st2);//eq~
+	}
+	@Test
+	public void testFunc3() {
+		assertNull(null);
+		assertNotNull(new String());
+	}
+	@Test
+	public void testFunc4() {
+		assertTrue(false);
+		assertFalse(false);
+	}
+	@Test
+	public void testFunc5() {
+		int[] arr1={1,2};
+		int[] arr2={1,2};
+		assertArrayEquals(arr1, arr2);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
